d5102921d7ad0d3eb2be1d4a2775a35d32a0b71c
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2024-12-01 20:27:17 +0700

    update portofolio sementara

--- src/js/script.js
+++ src/js/script.js
@@ -60,10 +60,17 @@ if (
 var slidePosition1 = 1;
 var slidePosition2 = 1;
 var slidePosition3 = 1;
+var slidePosition4 = 1;
+var slidePosition5 = 1;
+var slidePosition6 = 1;
+var slidePosition7 = 1;
 SlideShow1(slidePosition1);
 SlideShow2(slidePosition2);
-SlideShow3(slidePositio3);
-SlideShow3(slidePositio4);
+SlideShow3(slidePosition3);
+SlideShow4(slidePosition4);
+SlideShow5(slidePosition5);
+SlideShow6(slidePosition6);
+SlideShow7(slidePosition7);
 
 // forward/Back controls
 function plusSlides1(n) {
@@ -76,7 +83,16 @@ function plusSlides3(n) {
   SlideShow3((slidePosition3 += n));
 }
 function plusSlides4(n) {
-  SlideShow3((slidePosition3 += n));
+  SlideShow4((slidePosition4 += n));
+}
+function plusSlides5(n) {
+  SlideShow5((slidePosition5 += n));
+}
+function plusSlides6(n) {
+  SlideShow6((slidePosition6 += n));
+}
+function plusSlides7(n) {
+  SlideShow7((slidePosition7 += n));
 }
 
 //  images controls
@@ -90,7 +106,16 @@ function currentSlide3(n) {
   SlideShow3((slidePosition3 = n));
 }
 function currentSlide4(n) {
-  SlideShow3((slidePosition3 = n));
+  SlideShow4((slidePosition4 = n));
+}
+function currentSlide5(n) {
+  SlideShow5((slidePosition5 = n));
+}
+function currentSlide6(n) {
+  SlideShow6((slidePosition6 = n));
+}
+function currentSlide7(n) {
+  SlideShow7((slidePosition7 = n));
 }
 
 function SlideShow1(n) {
@@ -169,9 +194,66 @@ function SlideShow4(n) {
   slides[slidePosition4 - 1].style.display = "block";
   circles[slidePosition4 - 1].className += " enable";
 }
+function SlideShow5(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers5");
+  var circles = document.getElementsByClassName("dots5");
+  if (n > slides.length) {
+    slidePosition5 = 1;
+  }
+  if (n < 1) {
+    slidePosition5 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition5 - 1].style.display = "block";
+  circles[slidePosition5 - 1].className += " enable";
+}
+function SlideShow6(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers6");
+  var circles = document.getElementsByClassName("dots6");
+  if (n > slides.length) {
+    slidePosition6 = 1;
+  }
+  if (n < 1) {
+    slidePosition6 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition6 - 1].style.display = "block";
+  circles[slidePosition6 - 1].className += " enable";
+}
+function SlideShow7(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers7");
+  var circles = document.getElementsByClassName("dots7");
+  if (n > slides.length) {
+    slidePosition7 = 1;
+  }
+  if (n < 1) {
+    slidePosition7 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition7 - 1].style.display = "block";
+  circles[slidePosition7 - 1].className += " enable";
+}
 
 function myFunction1() {
-  var dots = document.getElementById("dots1");
+  var dots = document.getElementById("dot1");
   var moreText = document.getElementById("more1");
   var btnText = document.getElementById("myBtn1");
 
@@ -187,7 +269,7 @@ function myFunction1() {
 }
 
 function myFunction2() {
-  var dots = document.getElementById("dots2");
+  var dots = document.getElementById("dot2");
   var moreText = document.getElementById("more2");
   var btnText = document.getElementById("myBtn2");
 
@@ -203,7 +285,7 @@ function myFunction2() {
 }
 
 function myFunction3() {
-  var dots = document.getElementById("dots3");
+  var dots = document.getElementById("dot3");
   var moreText = document.getElementById("more3");
   var btnText = document.getElementById("myBtn3");
 
@@ -219,7 +301,52 @@ function myFunction3() {
 }
 
 function myFunction4() {
-  var dots = document.getElementById("dots4");
+  var dots = document.getElementById("dot4");
+  var moreText = document.getElementById("more4");
+  var btnText = document.getElementById("myBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Baca Selengkapnya";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Baca Lebih Sedikit";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction5() {
+  var dots = document.getElementById("dot4");
+  var moreText = document.getElementById("more4");
+  var btnText = document.getElementById("myBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Baca Selengkapnya";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Baca Lebih Sedikit";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction6() {
+  var dots = document.getElementById("dot4");
+  var moreText = document.getElementById("more4");
+  var btnText = document.getElementById("myBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Baca Selengkapnya";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Baca Lebih Sedikit";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction7() {
+  var dots = document.getElementById("dot4");
   var moreText = document.getElementById("more4");
   var btnText = document.getElementById("myBtn4");
 
